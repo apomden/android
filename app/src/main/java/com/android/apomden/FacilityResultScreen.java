006cb5c09e09a7f8d6b102d61ad5616e40adfce8
charlesagyemang <micnkru@gmail.com>
2019-09-24 11:12:02 +0000

    Feat: Implement listview plus logi and make it work

--- app/src/main/java/com/android/apomden/FacilityResultScreen.java
+++ app/src/main/java/com/android/apomden/FacilityResultScreen.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.android.apomden.Adapters.FacilityAdapter;
 import com.android.apomden.Utilities.Globall;
 
 public class FacilityResultScreen extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,21 @@ public class FacilityResultScreen extends AppCompatActivity {
         setContentView(R.layout.activity_facility_result_screen);
 
         Log.e("========sides====", String.valueOf(Globall.globallFacilities.size()));
+
+
+        listView = findViewById(R.id.facilitiesList);
+
+        FacilityAdapter facilityAdapter = new FacilityAdapter(this, 0, Globall.globallFacilities);
+        listView.setAdapter(facilityAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
+
 
     }
 }
