0277322e4fe47f8e70bc26249ffaccc575242f99
charlesagyemang <micnkru@gmail.com>
2019-09-25 02:12:35 +0000

    REF: delete some unnccessary shit

--- app/src/main/java/com/android/apomden/MainScreen.java
+++ app/src/main/java/com/android/apomden/MainScreen.java
@@ -7,7 +7,6 @@ import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import android.util.Log;
 import android.view.View;
